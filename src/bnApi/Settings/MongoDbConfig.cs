44d1e37026cc028899aa4e71c93c9613ac74c9cf
Hy0tic <vothanhdat278@gmail.com>
2024-06-19 01:40:54 -0400

    add Application User and Role

--- src/bnApi/Settings/MongoDbConfig.cs
+++ src/bnApi/Settings/MongoDbConfig.cs
@@ -4,7 +4,6 @@ public sealed class MongoDbConfig
 {
     public string Name { get; init; }
     public string Host { get; init; }
-    public int Port { get; init; }
     public string Username { get; init; }
     public string Password { get; init; }
     public string ConnectionString => $"mongodb+srv://{Username}:{Password}@{Host}/";
